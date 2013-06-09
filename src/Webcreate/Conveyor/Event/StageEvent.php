a891630c98b1ee19c65f555dfcb7ae24b282cdf1
fieg <jeroen@fieg.nl>
2013-06-09 22:39:50 +0200

    updated copyright headers

--- src/Webcreate/Conveyor/Event/StageEvent.php
+++ src/Webcreate/Conveyor/Event/StageEvent.php
@@ -1,8 +1,12 @@
 <?php
 
 /*
- * @author Jeroen Fiege <jeroen@webcreate.nl>
- * @copyright Webcreate (http://webcreate.nl)
+ * This file is part of the Conveyor package.
+ *
+ * (c) Jeroen Fiege <jeroen@webcreate.nl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
 namespace Webcreate\Conveyor\Event;
